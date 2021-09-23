4bbe5dc79fde91842726801a08bc2bf7f43504cf
Derek-Cruz <Derekcruz94@gmail.com>
2021-09-23 11:58:29 -0700

    Working on my render function. #1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ const $header = document.querySelector('header');
 const $welcomePage = document.querySelector('div[data-view="welcome-page"]');
 const $browsingPage = document.querySelector('div[data-view="browsing-page"]');
 const $getStartedButton = document.querySelector('.get-started-button-style');
+// const $userList = document.querySelector('#user-list');
+
+// function getData() {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://api.magicthegathering.io/v1/cards');
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function () {
+//     // console.log(xhr.response);
+
+//     for (const loop in xhr.response.cards) {
+//       const $newLi = document.createElement('li');
+//       $newLi.textContent = xhr.response.cards[loop].name;
+//       $userList.append($newLi);
+//     }
+//   });
+//   xhr.send();
+// }
+
+// getData();
 
 $getStartedButton.addEventListener('click', function (event) {
   $header.classList = 'header-background view';
@@ -9,22 +28,12 @@ $getStartedButton.addEventListener('click', function (event) {
   $browsingPage.classList = 'view';
 });
 
-const $userList = document.querySelector('#user-list');
+// function renderCards(card) {
+//   const $li = document.createElement('li');
+//   const $div = document.createElement('div');
+//   const $h2 = document.createElement('h2');
+//   const $img = document.createElement('img');
+//   const $button = document.createElement('button');
 
-function getData() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.magicthegathering.io/v1/cards');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    // console.log(xhr.response);
-
-    for (const loop in xhr.response.cards) {
-      const $newLi = document.createElement('li');
-      $newLi.textContent = xhr.response.cards[loop].name;
-      $userList.append($newLi);
-    }
-  });
-  xhr.send();
-}
-
-getData();
+//   $li.setAttribute();
+// }
