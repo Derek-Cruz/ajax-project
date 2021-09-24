b8a3bb7c3ecbb50da367367fc81b18fb7cde1dfe
Derek-Cruz <Derekcruz94@gmail.com>
2021-09-23 18:00:17 -0700

    Fixed render for cards that didnt have img. #1

--- js/main.js
+++ js/main.js
@@ -2,25 +2,23 @@ const $header = document.querySelector('header');
 const $welcomePage = document.querySelector('div[data-view="welcome-page"]');
 const $browsingPage = document.querySelector('div[data-view="browsing-page"]');
 const $getStartedButton = document.querySelector('.get-started-button-style');
-// const $userList = document.querySelector('#card-list');
+const $cardList = document.querySelector('#card-list');
 
-// function getData() {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://api.magicthegathering.io/v1/cards');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', function () {
-//     // console.log(xhr.response);
+function getData() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.magicthegathering.io/v1/cards');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log('results:', xhr.response);
+    for (var i = 0; i < xhr.response.cards.length; i++) {
+      var $cards = renderCards(xhr.response.cards[i]);
+      $cardList.appendChild($cards);
+    }
+  });
+  xhr.send();
+}
 
-//     for (const loop in xhr.response.cards) {
-//       const $newLi = document.createElement('li');
-//       $newLi.textContent = xhr.response.cards[loop].name;
-//       $userList.append($newLi);
-//     }
-//   });
-//   xhr.send();
-// }
-
-// getData();
+getData();
 
 $getStartedButton.addEventListener('click', function (event) {
   $header.classList = 'header-background view';
@@ -28,26 +26,34 @@ $getStartedButton.addEventListener('click', function (event) {
   $browsingPage.classList = 'view';
 });
 
-// function renderCards(card) {
-//   const $li = document.createElement('li');
-//   const $div = document.createElement('div');
-//   const $divTwo = document.createElement('div');
-//   const $h2 = document.createElement('h2');
-//   const $img = document.createElement('img');
-//   const $button = document.createElement('button');
+function renderCards(card) {
+  const $li = document.createElement('li');
+  const $div = document.createElement('div');
+  const $divTwo = document.createElement('div');
+  const $h2 = document.createElement('h2');
+  const $img = document.createElement('img');
+  const $button = document.createElement('button');
 
-//   $li.setAttribute('class', 'card-display-li');
-//   $h2.setAttribute('class', 'card-display-h2');
-//   $img.setAttribute('class', 'card-display-img');
-//   $img.setAttribute('src', './images/placeholder.jpg');
-//   $divTwo.setAttribute('class', 'card-display-button-div');
-//   $button.setAttribute('class', 'card-display-add');
+  $li.setAttribute('class', 'card-display-li');
+  $h2.setAttribute('class', 'card-display-h2');
+  $h2.textContent = card.name;
 
-//   $li.appendChild($div);
-//   $div.appendChild$($h2);
-//   $div.appendChild($img);
-//   $div.appendChild($divTwo);
-//   $divTwo.appendChild($button);
+  $img.setAttribute('class', 'card-display-img');
+  if (!card.imageUrl) {
+    $img.setAttribute('src', './images/placeholder.jpg');
+  } else {
+    $img.setAttribute('src', card.imageUrl);
+  }
 
-//   return $li;
-// }
+  $divTwo.setAttribute('class', 'card-display-button-div');
+  $button.setAttribute('class', 'card-display-add');
+  $button.textContent = 'Add';
+
+  $li.appendChild($div);
+  $div.appendChild($h2);
+  $div.appendChild($img);
+  $div.appendChild($divTwo);
+  $divTwo.appendChild($button);
+
+  return $li;
+}
